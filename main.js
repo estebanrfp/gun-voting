2379d40fc50ab3eebf74fe6a0450aa0e8bff6856
estebanrfp <estebanrfp@gmail.com>
2021-08-26 14:25:46 +0200

    add vite entry point file

---
+++ main.js
@@ -0,0 +1,17 @@
+import { registerSW } from 'virtual:pwa-register'
+
+import './lib/index.js'
+
+const updateSW = registerSW({
+  onNeedRefresh () {
+    // show a prompt to user
+    console.log('New content available, click on reload button to update.')
+    window.location.reload()
+  },
+  onOfflineReady () {
+    console.log('show a ready to work offline to user')
+    // show a ready to work offline to user
+  }
+})
+
+updateSW()
