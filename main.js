9906123acace2cee32da57a9fb968874bad24682
estebanrfp <estebanrfp@gmail.com>
2022-08-10 06:40:42 +0200

    update gun

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
-import { registerSW } from 'virtual:pwa-register'
+// import { registerSW } from 'virtual:pwa-register'
 
 import './lib/index.js'
 
-const updateSW = registerSW({
-  immediate: true,
-  onNeedRefresh () {
-    // show a prompt to user
-    console.log('New content available, click on reload button to update.')
-    window.location.reload()
-  },
-  onOfflineReady () {
-    console.log('show a ready to work offline to user')
-    // show a ready to work offline to user
-  }
-})
+// const updateSW = registerSW({
+//   immediate: true,
+//   onNeedRefresh () {
+//     // show a prompt to user
+//     console.log('New content available, click on reload button to update.')
+//     window.location.reload()
+//   },
+//   onOfflineReady () {
+//     console.log('show a ready to work offline to user')
+//     // show a ready to work offline to user
+//   }
+// })
 
-updateSW()
+// updateSW()
